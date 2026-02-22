f55dfbcbaee82e5ab89b9f0f25ba057e6895beb0
ghosheep5274-creator <ghosheep5274@gmail.com>
2026-02-23 01:25:37 +0900

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,9 +1,15 @@
 // songs.js - 純索引版
 const songLibrary = {
-    "mic_drop": {
-        title: "🎤 Deja Vu",
+    "deja_vu": {
+        title: "⭐ Deja Vu",
         videoId: "6g9IK4hYmYg",
         bpm: 81.5,
         file: "lyrics/deja_vu.json" // 指向檔案
+    },
+    "blue_spring": {
+        title: "🩵🌸 Blue Spring",
+        videoId: "pA75tY1NxcM",
+        bpm: 85,
+        file: "lyrics/blue_spring.json" // 指向檔案
     }
 };
